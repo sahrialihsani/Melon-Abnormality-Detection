6f7f8131a6c5c333cef288393be9cffe3368e1be
Sahrial Ihsani Ishak <67180039+sahrialihsani@users.noreply.github.com>
2024-01-23 03:56:49 +0700

    Update streamlit_yolov7.py

--- streamlit_yolov7.py
+++ streamlit_yolov7.py
@@ -175,7 +175,7 @@ class Streamlit_YOLOV7(SingleInference_YOLOV7):
         df['confidence'] = pd.to_numeric(df['confidence'], errors='coerce')
         self.image = None
 
-        st.json(json_array)
+        st.json(df)
 if __name__=='__main__':
     app=Streamlit_YOLOV7()
 
